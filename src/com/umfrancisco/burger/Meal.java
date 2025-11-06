6dd4746b811b392c5cb96e036fc2c0a6fb4d1bc3
umfrancisco <francis.guitler@gmail.com>
2025-11-06 15:30:40 -0300

    added enum to burger inner class

--- src/com/umfrancisco/burger/Meal.java
+++ src/com/umfrancisco/burger/Meal.java
@@ -1,9 +1,12 @@
 package com.umfrancisco.burger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 	
 	private double price = 5.0;
-	private Item burger;
+	private Burger burger;
 	private Item drink;
 	private Item side;
 	private double conversionRate;
@@ -14,18 +17,22 @@ public class Meal {
 	
 	public Meal(double conversionRate) {
 		this.conversionRate = conversionRate;
-		burger = new Item("regular", "burger");
+		burger = new Burger("regular");
 		drink = new Item("coke", "drink", 1.5);
 		side = new Item("fries", "side", 2.0);
 	}
 	
 	public double getTotal() {
-		double total = burger.price + drink.price + side.price;
+		double total = burger.getPrice() + drink.price + side.price;
 		return Item.getPrice(total, conversionRate);
 	}
 	
 	public String toString() {
 		return String.format("%s\n%s\n%s\n%26s$%.2f", burger, drink, side, "Total Due: ", getTotal());
+	}
+	
+	public void addToppings(String... selectedToppings) {
+		burger.addToppings(selectedToppings);
 	}
 	
 	private class Item {
@@ -50,6 +57,55 @@ public class Meal {
 		
 		private static double getPrice(double price, double rate) {
 			return price * rate;
+		}
+	}
+	
+	private class Burger extends Item {
+		
+		private enum Extra {
+			AVOCADO, BACON, CHEESE, KETCHUP, MAYO, PICKLES; 
+			
+			private double getPrice() {
+				if (this == AVOCADO) return 1.0;
+				if (this == BACON || this == CHEESE) return 1.5;
+				return 0.0;
+			}
+		}
+		
+		private List<Item> toppings = new ArrayList<>();
+		
+		Burger(String name) {
+			super(name, "burger", 5.0);
+		}
+		
+		public double getPrice() {
+			double total = super.price;
+			for (Item topping : toppings) {
+				total += topping.price;
+			}
+			return total;
+		}
+		
+		private void addToppings(String... selectedToppings) {
+			
+			for (String selectedTopping : selectedToppings) {
+				try {
+					Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+					toppings.add(new Item(topping.name(), "TOPPING", topping.getPrice()));					
+				} catch(IllegalArgumentException e) {
+					System.err.println("No topping found for "+selectedTopping);
+				}
+			}
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder itemized = new StringBuilder(super.toString());
+			for (Item topping : toppings) {
+				itemized.append("\n");
+				itemized.append(topping);
+			}
+			return itemized.toString();
 		}
 	}
 }
--- src/com/umfrancisco/burger/Store.java
+++ src/com/umfrancisco/burger/Store.java
@@ -3,6 +3,7 @@ package com.umfrancisco.burger;
 public class Store {
 	public static void main(String[] args) {
 		Meal regularMeal = new Meal();
+		regularMeal.addToppings("Ketchup", "Mayo", "Bacon", "Cheddar");
 		System.out.println(regularMeal+"\n");
 		
 		Meal USRegularMeal = new Meal(0.68);
