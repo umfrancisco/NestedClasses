844640964a74400ff9afd5cd398facdfcc878943
umfrancisco <francis.guitler@gmail.com>
2025-11-07 14:24:02 -0300

    added local class

--- src/com/umfrancisco/Main.java
+++ src/com/umfrancisco/Main.java
@@ -38,5 +38,46 @@ public class Main {
 		for (var e : storeEmployees) {
 			System.out.println(e);
 		}
+		
+		System.out.println("With pig latin names: ");
+		addPigLatinName(storeEmployees);
+	}
+	
+	public static void addPigLatinName(List<? extends StoreEmployee> list) {
+		
+		String lastName = "Piggy";
+		
+		class DecoratedEmployee extends StoreEmployee implements Comparable<DecoratedEmployee> {
+			private String pigLatinName;
+			private Employee originalInstance;
+			
+			public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+				this.pigLatinName = pigLatinName+" "+lastName;
+				this.originalInstance = originalInstance;
+			}
+			
+			@Override
+			public String toString() {
+				return originalInstance.toString()+" "+pigLatinName;
+			}
+
+			@Override
+			public int compareTo(DecoratedEmployee o) {
+				return pigLatinName.compareTo(o.pigLatinName);
+			}
+		}
+		
+		List<DecoratedEmployee> newList = new ArrayList<>(list.size()); 
+		
+		for (var employee : list) {
+			String name = employee.getName();
+			String pigLatin = name.substring(1)+name.charAt(0)+"ay";
+			newList.add(new DecoratedEmployee(pigLatin, employee));
+		}
+		
+		newList.sort(null);
+		for (var dEmployee : newList) {
+			System.out.println(dEmployee.originalInstance.getName()+" "+dEmployee.pigLatinName);
+		}
 	}
 }
