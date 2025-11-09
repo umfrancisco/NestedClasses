dbffdd18d0d30c545c790be09a80006dfce629fe
umfrancisco <francis.guitler@gmail.com>
2025-11-09 10:14:55 -0300

    added lambda expressions

---
+++ src/com/umfrancisco/RunMethods.java
@@ -0,0 +1,56 @@
+package com.umfrancisco;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import com.umfrancisco.domain.Employee;
+import com.umfrancisco.domain.EmployeeComparator;
+import com.umfrancisco.domain.StoreEmployee;
+
+public class RunMethods {
+	public static void main(String[] args) {
+		List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+				new StoreEmployee(10015, "Meg", 2019, "Target"),
+				new StoreEmployee(10515, "Joe", 2021, "Walmart"),
+				new StoreEmployee(10105, "Tom", 2020, "Macys"),
+				new StoreEmployee(10215, "Marty", 2018, "Walmart"),
+				new StoreEmployee(10322, "Bud", 2016, "Target")));
+		var c0 = new EmployeeComparator<StoreEmployee>();
+		var c1 = new Employee.EmployeeComparator<StoreEmployee>();
+		var c2 = new StoreEmployee().new StoreComparator<StoreEmployee>();
+		
+		class NameSort<T> implements Comparator<StoreEmployee> {
+			
+			@Override
+			public int compare(StoreEmployee o1, StoreEmployee o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		}
+		
+		var c3 = new NameSort<StoreEmployee>();
+		
+		var c4 = new Comparator<StoreEmployee>() {
+			@Override
+			public int compare(StoreEmployee o1, StoreEmployee o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		};
+		
+		sortIt(storeEmployees, c0);
+		sortIt(storeEmployees, c1);
+		sortIt(storeEmployees, c2);
+		sortIt(storeEmployees, c3);
+		sortIt(storeEmployees, c4);
+		sortIt(storeEmployees, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+		
+	}
+	
+	public static <T> void sortIt(List<T> list, Comparator<? super T> comparator) {
+		System.out.println("Sorting with comparator: "+comparator.toString());
+		list.sort(comparator);
+		for (var employee : list) {
+			System.out.println(employee);
+		}
+	}
+}
